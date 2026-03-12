f5715358cef4ee2ce5cc1b6cf69d117ee2a37d53
Jacob Cho <jacho@usc.edu>
2026-03-12 01:15:07 +0000

    Finished consumer

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,73 @@
+import pika
+import os
+
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(self, binding_key: str, exchange_name: str, queue_name: str):
+        # Save parameters to class variables
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        
+        # Call setupRMQConnection
+        self.connection = None
+        self.channel = None
+        
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+
+        # Establish Channel
+        self.channel = self.connection.channel()
+
+        # Create Queue if not already present
+        self.channel.queue_declare(queue=self.queue_name)
+
+        # Create the exchange if not already present
+        self.exchange = self.channel.exchange_declare(exchange=self.exchange_name)
+
+        # Bind Binding Key to Queue on the exchange
+        self.channel.queue_bind(
+            queue = self.queue_name,
+            routing_key = self.binding_key,
+            exchange = self.exchange_name,
+        )
+
+        # Set-up Callback function for receiving messages
+        self.channel.basic_consume(
+            queue = self.queue_name,
+            on_message_callback = self.on_message_callback,
+            auto_ack = False
+        )
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+        # Acknowledge message
+        channel.basic_ack(method_frame.delivery_tag, False)
+
+        #Print message (The message is contained in the body parameter variable)
+        print(f"Received message: {body.decode('utf-8')}")
+
+    def startConsuming(self) -> None:
+        # Print " [*] Waiting for messages. To exit press CTRL+C"
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+
+        # Start consuming messages
+        self.channel.start_consuming()
+    
+    def __del__(self) -> None:
+        # Print "Closing RMQ connection on destruction"
+        print("Closing RMQ connection on destruction")
+
+        # Close Channel
+        if self.channel is not None:
+            self.channel.close()
+
+        # Close Connection
+        if self.connection is not None:
+            self.connection.close()
